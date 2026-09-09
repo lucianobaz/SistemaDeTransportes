17b2f0befaa9dd293cb567e0fcc3574efcb599ba
lucianobaz <lb.proyectoss@gmail.com>
2026-09-09 17:29:39 -0300

    PARTE CATA

---
+++ Main.java
@@ -0,0 +1,69 @@
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+
+    //función que muestra la información de los paquetes (con sus asignaciones)
+    private static void mostrarInfoPaquete(Paquete p, double tipoCambio, Camionero c) {
+        System.out.println("Codigo: " + p.getCodigo());
+        System.out.println("Direccion: " + p.formatearDireccionEtiqueta());
+        System.out.println("Costo en pesos: " + p.calcularCostoEnvio());
+        System.out.println("Costo en dolares: " + p.calcularCostoEnDolares(tipoCambio));
+        System.out.println("Estado: " + p.consultarEstado());
+        System.out.println("Camionero: " + c.getNombre());
+        System.out.println("------------------------");
+    }
+
+    //función que asigna cada paquete a su camionero correspondiente
+    private static void asignarPaquete(Paquete p, Camionero c, List<Paquete> lista, List<Camionero> listaCamioneros) {
+        lista.add(p);
+        listaCamioneros.add(c);
+        c.getPaquetesAsignados().add(p.getCodigo());
+    }
+
+    public static void main(String[] args) {
+
+        //convierte costo de envío a dólares
+        double tipoCambio = 40.0;
+
+        //lista polimórfica de paquetes
+        List<Paquete> paquetes = new ArrayList<>();
+
+        //lista paralela: misma posición que "paquetes"
+        List<Camionero> camionerosDePaquetes = new ArrayList<>();
+
+        //precargados: departamento, camiones, camioneros y tipos de paquetes
+
+        //_____________________________________________________
+        Departamento montevideo = new Departamento("01", "Montevideo");
+
+        Camionero camionero1 = new Camionero("1.234.567-8", "Juan Perez", "099123456", "18 de Julio 1234", 35000.0, "Montevideo");
+
+        Camion camion1 = new Camion("SAB 1234", "Mercedes Benz", "Camión", 250.0);
+
+        Conduccion conduccion1 = new Conduccion(camionero1, camion1, LocalDate.of(2026, 3, 15));
+
+        PaqueteEstandar paq1 = new PaqueteEstandar("COD001", "Ropa", "Ana Gomez", "Calle Rivera 456", montevideo, 3.5, 200.0);
+        asignarPaquete(paq1, camionero1, paquetes, camionerosDePaquetes);
+
+        PaqueteFragil paq2 = new PaqueteFragil("COD002", "Vajilla", "Luis Diaz","Avenida Italia 789", montevideo, "Alto", 150.0);
+        asignarPaquete(paq2, camionero1, paquetes, camionerosDePaquetes);
+        //____________________________________________________
+        Departamento buenosAires = new Departamento("02", "Buenos Aires");
+
+        Camionero camionero2 = new Camionero("2.458.654-9", "Taylor Swift", "099436643", "Agraciada 356", 35000.0, "Buenos Aires");
+
+        PaqueteEstandar paq3 = new PaqueteEstandar("COD003", "Electronica", "Marcos Silva", "Calle Colon 789", buenosAires, 2.0, 300.0);
+        asignarPaquete(paq3, camionero2, paquetes, camionerosDePaquetes);
+        //______________________________________________________________
+       
+        //recorrido polimórfico, busca el camionero de cada paquete para mostrarlo
+        for (int i = 0; i < paquetes.size(); i++) {
+            Paquete p = paquetes.get(i);
+            Camionero camioneroDelPaquete = camionerosDePaquetes.get(i);
+            
+            mostrarInfoPaquete(p, tipoCambio, camioneroDelPaquete);
+        }
+    }
+}
