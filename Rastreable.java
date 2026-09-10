8a82610ebcbd402a5e144063545d42da20b8ed00
lucianobaz <lb.proyectoss@gmail.com>
2026-09-10 15:44:02 -0300

    Codigo Finalizado y Comentado

--- Rastreable.java
+++ Rastreable.java
@@ -3,6 +3,7 @@
 // Define el contrato obligatorio para todos los paquetes del sistema.
 // Obliga a implementar la consulta de estado y la generación del código único de rastreo.
 // ===================================================================
+
 public interface Rastreable {
     String consultarEstado();
     String generarCodigoRastreo();
